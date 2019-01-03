b02b7ac9ff3863d3b8e32b90638c8734116b8b4b
Matthieu <contact@le-web-developpeur.com>
2019-01-03 14:55:06 +0100

    Composant security, entité user et login form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,11 +6,44 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Categorie;
 use App\Entity\Article;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    //attribut pour stocker l'encoder
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder){
+        //lors de l'instanciation, on stocke dans l'attribut encoder, l'objet qui va nous permettre d'encoder les mdp
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+
+        //création de 5 users
+        for($i=1;$i<=5;$i++){
+
+            $user = new User();
+            $user->setUsername('Toto' . $i);
+            $user->setEmail('toto' . $i . '@toto.to');
+            if($i=== 1){//je veux que toto1 aie le rôle Admin
+                $roles = ['ROLE_USER', 'ROLE_ADMIN'];
+            }
+            else{
+                $roles =['ROLE_USER'];
+            }
+            $user->setRoles($roles);
+
+            $plainPassword = 'toto';
+            $mdpencoded = $this->encoder->encodePassword($user, $plainPassword);
+            $user->setPassword($mdpencoded);
+
+            $manager->persist($user);
+
+        }
+
         //on va créer 10 catégories
     	for($i=1;$i<=10;$i++){
 
